54127e8d742d1004a5fece353e4c304263c426c3
Administrator <liu123456>
2017-02-23 19:25:35 +0800

    update mysql conns 2017/02/23

--- src/loginServ.php
+++ src/loginServ.php
@@ -1,6 +1,7 @@
 <?php
     header("Content-type: text/html; charset=utf-8");
-    $conn = new mysqli("localhost" , "root" , "0000" , "myhtmldb");
+    //$conn = new mysqli("localhost" , "root" , "0000" , "myhtmldb");
+    $conn = new mysqli("bdm264098108.my3w.com" , "bdm264098108" , "liu123456" , "bdm264098108_db");
     mysqli_query($conn,"SET NAMES 'UTF8'");
     $username = $_REQUEST['username'];
     $password = md5($_REQUEST['password']);
