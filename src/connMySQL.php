4073d10248946c8769188b98dc6c3dbf52c66da0
LiuWeidon <liu18443101163@163.com>
2017-04-24 14:18:35 +0800

    2017/04/24 14:18

--- src/connMySQL.php
+++ src/connMySQL.php
@@ -5,5 +5,6 @@
             //$conn = new mysqli("bdm264098108.my3w.com" , "bdm264098108" , "liu123456" , "bdm264098108_db");
             $conn = new mysqli("localhost" , "root" , "0000" , "manager");
             return $conn;
+
         }
     }
